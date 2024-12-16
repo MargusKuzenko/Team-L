a7ee726037df590e95354a86fd514a6e812dbdf6
MargusKuzenko <Margusk10@gmail.com>
2024-12-16 16:43:33 +0200

    rough server base (not working)

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignupView from '../views/SignupView.vue'
+import LoginView from '../views/LoginView.vue'
+import ContactusView from '../views/ContactusView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    beforeEnter: async(to, from, next) => {
+      let authResult = await auth.authenticated();
+      if (!authResult) {
+          next('/login')
+      } else {
+          next();
+      }
+  }
   },
   {
     path: '/Signup',
     name: 'Signup',
-    component: () => import(/* webpackChunkName: "Signup" */ '../views/SignupView.vue')
+    component: SignupView
+  },
+  {
+    path: "/login",
+    name: "LogIn",
+    component: LoginView,
+  },
+  {
+      path: "/contactus",
+      name: "ContactUs",
+      component: ContactusView
   }
 ]
 
